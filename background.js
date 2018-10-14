71737b5e0322b58826b8ad42a4321beecf4dc08d
nkhachan <noopur.khachane@gmail.com>
2018-10-14 14:40:13 -0700

    update website to switch backgrounds

--- background.js
+++ background.js
@@ -3,9 +3,13 @@ document.getElementById("backgroundcheckbox").addEventListener("change", changeB
 function changeBackground(event){
   var checkbox = event.target;
   if (checkbox.checked) {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundImage =  "url('img/sad.png')";
+    //document.body.style.backgroundColor =  "red";
+    document.body.style.backgroundSize = "cover";
   }
   else {
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundImage =  "url('img/happy.png')";
+    //document.body.style.backgroundColor =  "blue";
+    document.body.style.backgroundSize = "cover";
   }
 }
